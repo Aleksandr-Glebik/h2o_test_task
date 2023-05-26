68b678cf65cbce1f9e4ee4f3de2772b74d4b2963
Aleksandr-Glebik <glebikaa@mail.ru>
2023-05-26 21:16:35 +0300

    feat: add markup and style for comp UserInfo

--- src/Pages/UsersPage/HeaderUsersPage/HeaderUserPage.tsx
+++ src/Pages/UsersPage/HeaderUsersPage/HeaderUserPage.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from './HeaderUserPage.module.scss'
 import BtnUserPageNav from '../../../components/BtnUserPageNav/BtnUserPageNav'
 import NavUsersPage from '../NavUsersPage/NavUsersPage'
+import UserInfo from '../UserInfo/UserInfo'
 
 const HeaderUserPage: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const HeaderUserPage: React.FC = () => {
             <BtnUserPageNav direction={'left'}/>
         </div>
         <NavUsersPage />
+        <UserInfo />
     </header>
   )
 }
--- src/Pages/UsersPage/UserInfo/UserInfo.tsx
+++ src/Pages/UsersPage/UserInfo/UserInfo.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import styles from './UserInfo.module.scss'
+import srcUserPhoto from '../../../assets/img/userPhoto.png'
+import srcIcon from '../../../assets/img/btnUserPageNav.svg'
 
 const UserInfo: React.FC = () => {
   return (
-    <div>UserInfo</div>
+    <div className={styles.container}>
+      <img
+        src={srcUserPhoto}
+        alt='user'
+        className={styles.img}
+      />
+      <div className={styles.textContent}>
+        <h3 className={styles.title}>Kristina</h3>
+        <p className={styles.text}>менеджер продаж</p>
+      </div>
+      <img
+        src={srcIcon}
+        alt='triangle'
+        className={styles.icon}
+      />
+    </div>
   )
 }
 
