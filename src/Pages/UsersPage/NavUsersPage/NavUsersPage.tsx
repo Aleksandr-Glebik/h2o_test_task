79dee0cb9dbcce9258549105d1bd7e1b0a125650
Aleksandr-Glebik <glebikaa@mail.ru>
2023-05-26 12:51:25 +0300

    feat: add styles for NavUserPage for scroll-x nav bar

--- src/Pages/UsersPage/NavUsersPage/NavUsersPage.tsx
+++ src/Pages/UsersPage/NavUsersPage/NavUsersPage.tsx
@@ -3,14 +3,14 @@ import styles from './NavUsersPage.module.scss'
 
 const NavUsersPage: React.FC = () => {
   return (
-    <div className={styles.container}>
+    <nav className={styles.container}>
       <ul className={styles.list}>
         <li className={styles.item}>База анкет сотрудников</li>
         <li className={styles.item}>Общая база сотрудников</li>
         <li className={styles.item}>База сотрудников</li>
         <li className={styles.item}>Календарь сотрудников</li>
       </ul>
-    </div>
+    </nav>
   )
 }
 
