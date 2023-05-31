007f26610d2a87b57c0e62d84be445ceed4f76e6
Aleksandr-Glebik <glebikaa@mail.ru>
2023-05-31 11:04:16 +0300

    feat: deploy projecr on gitHub, add btn redirect on home page for NotFoundPage

--- src/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import clsx from 'clsx'
 import styles from './NotFoundPage.module.scss'
+import { useNavigate } from 'react-router-dom'
 
 const NotFoundBlock: React.FC = () => {
+  const navigate = useNavigate()
+
+  const redirectHomePage = () => {
+    navigate('/')
+  }
+
   return (
     <div className={styles.root}>
         <h1 >
@@ -12,6 +20,12 @@ const NotFoundBlock: React.FC = () => {
         <p className={styles.text}>
             Данная страница не найдена
         </p>
+        <button
+          className={clsx(styles.btn, styles.notFoundBtn)}
+          onClick={redirectHomePage}
+        >
+          Перейти на главную страницу
+        </button>
     </div>
   )
 }
